ade7ff5f84f4857fa96e089d2580d0af3f7720ab
Ben Merlotti <benmerlotti@Bens-Laptop.local>
2024-11-13 16:08:26 -0600

    compelted get walkers by city

--- client/src/Walkers.jsx
+++ client/src/Walkers.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
-import { getCities, getWalkers } from "./apiManager";
+import { getCities, getCityWalkers, getWalkers } from "./apiManager";
 
 export const Walkers = () => {
   const [allWalkers, setAllWalkers] = useState([]);
   const [allCities, setAllCities] = useState([]);
+  const [allCityWalkers, setAllCityWalkers] = useState([]);
+  const [filteredWalkers, setFilteredWalkers] = useState([]);
 
   useEffect(() => {
     getWalkers().then((data) => {
       setAllWalkers(data);
+      setFilteredWalkers(data);
     });
   }, []);
 
@@ -17,11 +20,46 @@ export const Walkers = () => {
     });
   }, []);
 
+  useEffect(() => {
+    getCityWalkers().then((data) => {
+      setAllCityWalkers(data);
+    });
+  }, []);
+
+  const handleFilterByCity = (event) => {
+    const chosenCity = parseInt(event.target.value);
+    if (chosenCity) {
+      const walkerIdsInCity = allCityWalkers
+        .filter((cw) => cw.cityId == chosenCity)
+        .map((cw) => cw.walkerId);
+
+      const filtered = allWalkers.filter((w) => walkerIdsInCity.includes(w.id));
+
+      setFilteredWalkers(filtered);
+    } else {
+      setFilteredWalkers(allWalkers);
+    }
+  };
+
   return (
     <div>
-      {allWalkers.map((w) => {
-        return <li>{`${w.name}`}</li>;
-      })}
+      <div>
+        <select onChange={handleFilterByCity}>
+          <option value="">All Walkers</option>
+          {allCities.map((c) => {
+            return (
+              <option key={c.id} value={c.id}>
+                {c.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div>
+        {filteredWalkers.map((w) => {
+          return <li key={w.id}>{`${w.name}`}</li>;
+        })}
+      </div>
     </div>
   );
 };
